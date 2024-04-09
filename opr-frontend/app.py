62e15227dd1a7a490d4ddbcc6e9f37543f414597
Forrest Beck <forrest.beck@da.org>
2024-04-09 08:57:59 -0400

    reindex markers

--- opr-frontend/app.py
+++ opr-frontend/app.py
@@ -5,21 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('view_markers.html')
+    return redirect(url_for('view_markers'))
 
 
 @app.route('/edit_marker/<int:marker_id>')
 def edit_marker(marker_id):
-    # Fetch marker data based on the marker_id
-    # You can pass the marker data to the template
     return render_template('edit_marker.html', marker_id=marker_id)
 
 
 @app.route('/save_markers', methods=['POST'])
 def save_markers():
     markers = json.loads(request.form['markers'])
+    # re-index markers
+    new_index = 0
+    new_markers = {}
+    for i in markers:
+        temp = markers[i]
+        new_markers[new_index] = temp
+        new_index += 1
     with open('markers.json', 'w') as f:
-        json.dump(markers, f)
+        json.dump(new_markers, f)
     return redirect(url_for('view_markers'))
 
 
