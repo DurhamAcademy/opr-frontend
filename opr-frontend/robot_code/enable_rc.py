0e8176ac1addc01e3e542d5a7d836928a24bfbde
Forrest Beck <forrest.beck@da.org>
2024-05-15 23:02:46 -0400

    daemon to control rc or auto

--- opr-frontend/robot_code/enable_rc.py
+++ opr-frontend/robot_code/enable_rc.py
@@ -5,6 +5,9 @@ controller = nes.Nes()
 drive = motor_driver.Motor()
 
 while True:
-    left_speed, right_speed = controller.wpm_controller(controller.snes_input())
-    drive.set_left_speed(left_speed)
-    drive.set_right_speed(right_speed)
+    try:
+        left_speed, right_speed = controller.wpm_controller(controller.snes_input())
+        drive.set_left_speed(left_speed)
+        drive.set_right_speed(right_speed)
+    finally:
+        drive.cleanup()
