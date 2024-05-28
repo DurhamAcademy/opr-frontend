b08f30b46390a8a21b5e132fff0d68c3eaeadf36
Forrest Beck <forrest.beck@da.org>
2024-05-28 10:42:40 -0400

    trying remote control

--- opr-frontend/app.py
+++ opr-frontend/app.py
@@ -290,13 +290,18 @@ def handle_keyup(data):
 @app.route('/remote_control')
 @login_required
 def remote_control():
-    
-    # disable remote control
-    disable_robot_code()
-    from robot_code import motor_driver
-    # globals are bad so make this better
-    global md
-    md = motor_driver.Motor()
+    try:
+        # disable remote control
+        disable_robot_code()
+    except:
+        print("nothing to kill")
+    try:
+        from robot_code import motor_driver
+        # globals are bad so make this better
+        global md
+        md = motor_driver.Motor()
+    except:
+        print("cannot connect to motor driver")
 
     return render_template('remote_control.html')
 
